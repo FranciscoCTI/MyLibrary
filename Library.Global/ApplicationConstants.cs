460589f7ffa3f6974f47d0258a3a29b3b87fee04
FranciscoCTI <franciscofcch@gmail.com>
2025-03-05 17:34:45 -0300

    #Separated connection string data.

--- Library.Global/ApplicationConstants.cs
+++ Library.Global/ApplicationConstants.cs
@@ -5,7 +5,12 @@
     /// </summary>
     public static class ApplicationConstants
     {
-        public const string myConstant = "Constant value";
-        public const string MongoConnectionString = "mongodb+srv://francisco:IN3ihAfUZ5LWrFko@democluster.w6bc9.mongodb.net/?retryWrites=true&w=majority&appName=DemoCluster";
+        public const string MongoUserName = "francisco";
+
+        public const string MongoClusterPassword = "IN3ihAfUZ5LWrFko";
+
+        public static readonly string MongoConnectionString =
+            String.Format("mongodb+srv://{0}:{1}@democluster.w6bc9.mongodb.net/?retryWrites=true&w=majority&appName=DemoCluster",
+                MongoUserName, MongoClusterPassword);
     }
 }
